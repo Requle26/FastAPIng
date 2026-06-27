32de4756728227d040d9dc283e81e89b1cb74d9a
Requle26 <seongilgim325@gmail.com>
2026-06-27 13:33:47 +0900

    sqlite_basic01

---
+++ DATABASE/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import DeclarativeBase
+
+class Base(DeclarativeBase):
+    pass
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    age = Column(Integer)
